05c5a02ae6d92e548186a00158e05168216164bc
Matt Pavlovich <meacreatio@gmail.com>
2017-08-13 12:50:17 -0700

    add dAppDemo

---
+++ dAppDemo/client/main.js
@@ -0,0 +1,26 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+});
+
+Template.hello.helpers({
+  counter() {
+    web3.eth.getBalance("0xf1EBFABE214Ca7fd12909e9737264f910c2a7081", function(error, result) {
+      if (!error) {
+        alert(result);
+      }
+    })
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
